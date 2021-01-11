a9e3413342e65e152c2d69b07e8e1526ed24c9f3
Nick Pachulski <nick@pachulski.me>
2021-01-11 15:00:57 -0500

    Sort episodes: newest at top to oldest down bottom

--- src/Episodes/index.tsx
+++ src/Episodes/index.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from "react"
+import React, { useMemo, useEffect } from "react"
 import { FlatList, View } from "react-native"
 import { useSafeArea } from "react-native-safe-area-context"
+import parseISO from "date-fns/parseISO"
 import { useNavigation } from "@react-navigation/native"
 import { RouteProp } from "@react-navigation/native"
 import Loading from "../Loading"
@@ -9,6 +10,11 @@ import HeaderBarWithBackButton from "./HeaderBarWithBackButton"
 import Episode from "./Episode"
 import usePodcastFromRssFeed from "../hooks/usePodcastFromRssFeed"
 import useStyles from "./useStyles"
+
+const newestEpisodesFirst = (episodes: Episode[]) =>
+  Array.from(episodes).sort((e1, e2) =>
+    parseISO(e1.published_at) > parseISO(e2.published_at) ? -1 : 1,
+  )
 
 type EpisodesProps = {
   route: RouteProp<RouteParams, "Episodes">
@@ -22,6 +28,14 @@ const Episodes = ({ route }: EpisodesProps) => {
   const rssFeedUrl = route.params.podcastSearchResult.rssFeedUrl
 
   const { podcast, abortController } = usePodcastFromRssFeed({ rssFeedUrl })
+
+  const episodes = useMemo(
+    () =>
+      podcast && podcast.episodes
+        ? newestEpisodesFirst(podcast.episodes)
+        : undefined,
+    [podcast],
+  )
 
   const keyExtractor = <T,>(_: T, position: number) => position.toString()
 
@@ -37,13 +51,13 @@ const Episodes = ({ route }: EpisodesProps) => {
     <View testID="Episodes" style={styles.container}>
       <View style={{ height: insets.top }} />
       <HeaderBarWithBackButton goBack={navigation.goBack} />
-      {podcast ? (
+      {episodes ? (
         <FlatList
           style={styles.container}
           scrollIndicatorInsets={{ right: 1 }}
           ListHeaderComponent={<PodcastDescription podcast={podcast} />}
           ListFooterComponent={<View style={{ height: 30 }} />}
-          data={podcast.episodes}
+          data={episodes}
           keyExtractor={keyExtractor}
           renderItem={({ item: episode }) => <Episode episode={episode} />}
         />
