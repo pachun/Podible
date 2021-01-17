34c9b0109d0183e022df57522c671dd710ffc4c1
Nick Pachulski <nick@pachulski.me>
2021-01-17 18:07:18 -0500

    Customize loading spinner image and animation

--- src/Loading/index.tsx
+++ src/Loading/index.tsx
@@ -1,11 +1,26 @@
-import React, { ReactElement } from "react"
-import { ActivityIndicator, View } from "react-native"
+import React, { ReactElement, useRef, useState, useEffect } from "react"
+import { Image, View } from "react-native"
+import * as Animatable from "react-native-animatable"
 import useColorScheme from "../hooks/useColorScheme"
 import colorSchemes from "../colorSchemes"
+import logo from "../../assets/logo.png"
 
 const Loading = (): ReactElement => {
   const colorSchemeName = useColorScheme()
   const colorScheme = colorSchemes[colorSchemeName]
+
+  const loadingIconRef = useRef<Animatable.Image & Image>(null)
+  const [iteration, setIteration] = useState(0)
+
+  useEffect(() => {
+    const bounceInAndOut = async () => {
+      loadingIconRef.current && (await loadingIconRef.current.flipInX())
+      loadingIconRef.current && (await loadingIconRef.current.flipOutY())
+      loadingIconRef.current && setIteration(iteration + 1)
+    }
+    bounceInAndOut()
+  }, [iteration])
+
   return (
     <View
       style={{
@@ -15,7 +30,12 @@ const Loading = (): ReactElement => {
         backgroundColor: colorScheme.background,
       }}
     >
-      <ActivityIndicator size="large" color={colorScheme.tableHeader} />
+      <Animatable.Image
+        ref={loadingIconRef}
+        source={logo}
+        style={{ width: 100, height: 100 }}
+      />
+      <View style={{ height: 50 }} />
     </View>
   )
 }
