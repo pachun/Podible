195581d916e82fc851a539bac3df15efa3e1dcce
Nick Pachulski <nick@pachulski.me>
2021-02-09 19:51:44 -0500

    Fix playback issue
    Sometimes when resuming listening to an episode which has been started,
    particularly when you’re resuming at a point long into the episode (say,
    several hours into it), the resume just restarts the listener at 0:00.
    
    I think this is because awaiting TrackPlayer.play doesn’t seem to work.
    
    We avoid the need for awaiting TrackPlayer.play by using an event
    listener which listens for a play event.

--- src/shared/trackPlayerHelpers.ts
+++ src/shared/trackPlayerHelpers.ts
@@ -3,6 +3,34 @@ import TrackPlayer, { Track } from "react-native-track-player"
 const isCurrentEpisode = async (episode: Episode) =>
   (await TrackPlayer.getCurrentTrack()) === episode.audio_url
 
+const playAnEpisodeWhichHasNotBeenStarted = async (episode: Episode) => {
+  await TrackPlayer.stop()
+  await TrackPlayer.add([trackPlayerTrackFromEpisode(episode)])
+  await TrackPlayer.skip(episode.audio_url)
+  await TrackPlayer.play()
+}
+
+const globalAny: any = global
+const playAnEpisodeWhichHasBeenStarted = async (episode: Episode) => {
+  const originalVolume = await TrackPlayer.getVolume()
+  await TrackPlayer.setVolume(0)
+  await TrackPlayer.stop()
+  await TrackPlayer.add([trackPlayerTrackFromEpisode(episode)])
+  await TrackPlayer.skip(episode.audio_url)
+
+  globalAny.trackPlayerPlaybackEventListener = TrackPlayer.addEventListener(
+    "playback-state",
+    async stateObject => {
+      if (stateObject.state === TrackPlayer.STATE_PLAYING) {
+        await TrackPlayer.seekTo(episode.seconds_listened_to)
+        await TrackPlayer.setVolume(originalVolume)
+        globalAny.trackPlayerPlaybackEventListener.remove()
+      }
+    },
+  )
+  await TrackPlayer.play()
+}
+
 export const trackPlayerTrackFromEpisode = (episode: Episode): Track => ({
   id: episode.audio_url,
   title: episode.title,
@@ -15,23 +43,10 @@ export const play = async (episode: Episode): Promise<void> => {
   if (await isCurrentEpisode(episode)) {
     TrackPlayer.play()
   } else {
-    const originalVolume = await TrackPlayer.getVolume()
-    await TrackPlayer.setVolume(0)
-
-    await TrackPlayer.stop()
-    await TrackPlayer.add([trackPlayerTrackFromEpisode(episode)])
-    await TrackPlayer.skip(episode.audio_url)
-
-    // begin playing at a seeked-to position (jank below accounts for the open iOS issue):
-    // https://github.com/react-native-kit/react-native-track-player/issues/387
-    await TrackPlayer.play()
     if (episode.seconds_listened_to > 0) {
-      await setTimeout(async () => {
-        await TrackPlayer.seekTo(episode.seconds_listened_to)
-        setTimeout(async () => await TrackPlayer.setVolume(originalVolume), 500)
-      }, 1200)
+      await playAnEpisodeWhichHasBeenStarted(episode)
     } else {
-      await TrackPlayer.setVolume(originalVolume)
+      await playAnEpisodeWhichHasNotBeenStarted(episode)
     }
   }
 }
