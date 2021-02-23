a5600cd9dc73ec3db9428bea6632a652e5330f1e
Nick Pachulski <nick@pachulski.me>
2021-02-23 11:08:11 -0500

    Fix bug causing resume play to buffer indefinitely
    When listening to a track, if you pause the track, drop the Now Playing
    Screen (dismiss it down off the screen), minimize the app, reopen the
    app and then tap the play button on the MiniPlayer component at the
    bottom of the screen, the play button turns into a loading spinner
    indefinitely as the playback state switches to “Buffering” indefinitely.
    
    This commit prevents the infinite buffering state from occurring and
    allows resuming playback from the MiniPlayer in the way which you’d
    expect.

--- src/PlayPauseButton/index.tsx
+++ src/PlayPauseButton/index.tsx
@@ -1,9 +1,16 @@
-import React, { ReactElement, useContext } from "react"
+import React, {
+  ReactElement,
+  useContext,
+  useState,
+  useCallback,
+  useEffect,
+} from "react"
 import { ActivityIndicator, TouchableOpacity } from "react-native"
 import * as Haptics from "expo-haptics"
 import { Ionicons } from "@expo/vector-icons"
 import TrackPlayer from "react-native-track-player"
 import { PodibleContext } from "../Provider"
+import { trackPlayerTrackFromEpisode } from "../shared/trackPlayerHelpers"
 import useColorScheme from "../hooks/useColorScheme"
 
 interface PlayPauseButtonProps {
@@ -17,18 +24,73 @@ const PlayPauseButton = ({
 }: PlayPauseButtonProps): ReactElement => {
   const colorScheme = useColorScheme()
 
-  const { playbackState } = useContext(PodibleContext)
+  const { currentlyPlayingEpisode, playbackState } = useContext(PodibleContext)
 
   const pauseEpisode = () => {
     Haptics.impactAsync()
     TrackPlayer.pause()
   }
 
-  const playEpisode = () => {
+  const [isSeekingAfterPlaying, setIsSeekingAfterPlaying] = useState(false)
+  const [originalVolume, setOriginalVolume] = useState(1.0)
+
+  const playEpisode = async () => {
     Haptics.impactAsync()
-    TrackPlayer.play()
     onPlay()
+    const isCurrentTrack =
+      currentlyPlayingEpisode.audio_url ===
+      (await TrackPlayer.getCurrentTrack())
+    if (currentlyPlayingEpisode) {
+      if (isCurrentTrack) {
+        await TrackPlayer.play()
+      } else if (currentlyPlayingEpisode.seconds_listened_to === 0) {
+        await TrackPlayer.stop()
+        await TrackPlayer.add([
+          trackPlayerTrackFromEpisode(currentlyPlayingEpisode),
+        ])
+        await TrackPlayer.skip(currentlyPlayingEpisode.audio_url)
+        await TrackPlayer.play()
+      } else {
+        setIsSeekingAfterPlaying(true)
+        setOriginalVolume(await TrackPlayer.getVolume())
+        await TrackPlayer.setVolume(0)
+
+        await TrackPlayer.stop()
+        await TrackPlayer.add([
+          trackPlayerTrackFromEpisode(currentlyPlayingEpisode),
+        ])
+        await TrackPlayer.skip(currentlyPlayingEpisode.audio_url)
+        await TrackPlayer.play()
+      }
+    }
   }
+
+  const seekAfterPlaying = useCallback(() => {
+    const seekAfterPlayingListener = TrackPlayer.addEventListener(
+      "playback-state",
+      async (event: TrackPlayerEvent) => {
+        if (
+          isSeekingAfterPlaying &&
+          event.state === TrackPlayer.STATE_PLAYING
+        ) {
+          setIsSeekingAfterPlaying(false)
+          await TrackPlayer.seekTo(currentlyPlayingEpisode.seconds_listened_to)
+          await TrackPlayer.play()
+          setTimeout(() => {
+            TrackPlayer.setVolume(originalVolume)
+          }, 200)
+        }
+      },
+    )
+
+    return () => seekAfterPlayingListener.remove()
+  }, [
+    currentlyPlayingEpisode.seconds_listened_to,
+    originalVolume,
+    isSeekingAfterPlaying,
+  ])
+
+  useEffect(() => seekAfterPlaying(), [seekAfterPlaying])
 
   return (
     <>
